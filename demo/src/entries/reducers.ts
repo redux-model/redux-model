d83a901448fcc40d43a2ea893dbd0d53b2a179db
范文华 <531362022@qq.com>
2019-07-13 16:26:33 +0800

    refactor: Move demo to new repo

--- demo/src/entries/reducers.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers, Reducer } from 'redux';
-import { reducers } from '../models';
-import { EnhanceState } from '../../../index';
-
-declare global {
-  type RootState = Readonly<ReturnType<typeof rootReducers>>;
-}
-
-export const rootReducers: Reducer<EnhanceState<typeof reducers>> = combineReducers(reducers);
--- demo/src/models/index.ts
+++
@@ -1,9 +0,0 @@
-import { counterModel } from './CounterModel';
-import { npmInfoModel } from './NpmInfoModel';
-import { summaryModel } from './SummaryModel';
-
-export const reducers = {
-  ...counterModel.register(),
-  ...npmInfoModel.register(),
-  ...summaryModel.register(),
-};
