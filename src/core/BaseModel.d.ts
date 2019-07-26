f3da52491a13fa2b11d75d4e1976ea7a935bd069
范文华 <531362022@qq.com>
2019-07-26 18:08:39 +0800

    docs: Add more introduction in .d.ts file

--- src/core/BaseModel.d.ts
+++ src/core/BaseModel.d.ts
@@ -22,24 +22,30 @@ type EnhanceNormalPayload<A> = A extends (...args: any[]) => ActionNormal<infer 
 
 export declare abstract class BaseModel<Data = null> {
     static middlewareName: string;
+
+    // `const loading = a.action.useLoading() || b.action.useLoading()` is not allowed in hooks.
+    // But you can use this static method:
+    // `const loading = Model.isLoading(a.action.useLoading(), b.action.useLoading())`;
     static isLoading(...fromUseLoading: boolean[]): boolean;
 
     constructor(instanceName?: string);
 
     register(): Reducers;
 
+    // Remember: You can only use it in function component. Make sure React version >=16.8 and react-redux version >=7.1.0
     useData<T = Data>(filter?: (data: Data) => T): T;
+    // Remember: You can only use it in react-redux.connect() method.
     connectData(): Data;
 
     protected actionNormal<A extends (...args: any[]) => ActionNormal<Payload>, Payload = EnhanceNormalPayload<A>>(config: NormalActionParam<Data, A, Payload>): NormalAction<Data, A, Payload>;
 
-    // When meta=false
+    // Case meta is false. We will never create meta reducer for this action.
     protected actionRequest<A extends (...args: any[]) => FetchHandle<Response, Payload>, Response = EnhanceResponse<A>, Payload = EnhancePayload<A>>(config: RequestActionParamNoMeta<Data, A, Response, Payload>): RequestAction<Data, A, Response, Payload>;
 
-    // When meta is undefined or true.
+    // Case meta is undefined or true. we will automatically register meta reducer.
     protected actionRequest<A extends (...args: any[]) => FetchHandle<Response, Payload>, Response = EnhanceResponse<A>, Payload = EnhancePayload<A>>(config: RequestActionParamWithMeta<Data, A, Response, Payload>): RequestActionWithMeta<Data, A, Response, Payload>;
 
-    // When meta is the key of payload.
+    // Case meta is one of payload's key. we will automatically register metas reducer.
     protected actionRequest<A extends (...args: any[]) => FetchHandle<Response, Payload>, Response = EnhanceResponse<A>, Payload = EnhancePayload<A>>(config: RequestActionParamWithMetas<Data, A, Response, Payload>): RequestActionWithMetas<Data, A, Response, Payload>;
 
     protected actionThunk<A extends (...args: any[]) => ThunkAction<any, any, any, Action>>(action: A): (...args: Parameters<A>) => ReturnType<ReturnType<A>>;
