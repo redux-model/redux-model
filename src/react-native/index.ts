7698830e3b30e6ac885b7a0bd3ceeb529b9627f8
范文华 <531362022@qq.com>
2019-10-12 16:17:08 +0800

    feat: Export more type definition for HttpService

--- src/react-native/index.ts
+++ src/react-native/index.ts
@@ -1,8 +1,8 @@
 export { Model } from './Model';
 export { HttpService } from './HttpService';
-export { HttpError, HttpCanceler } from './types';
+export { HttpError, HttpCanceler, ActionRequest } from './types';
 
 export { METHOD } from '../core/utils/method';
 export { HTTP_STATUS_CODE } from '../core/utils/httpStatusCode';
-export { Effects, Meta, Metas, HttpTransform } from '../core/utils/types';
+export { Effects, Meta, Metas, HttpTransform, ActionResponse } from '../core/utils/types';
 export { getStore, createReduxStore  } from '../core/utils/createReduxStore';
