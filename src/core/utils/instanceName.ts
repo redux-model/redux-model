561db6eb13aaa22d3b7c38c6782e921b8ce64f7d
范文华 <531362022@qq.com>
2019-10-18 17:22:00 +0800

    fix: Compatible with compression in dev environment

--- src/core/utils/instanceName.ts
+++ src/core/utils/instanceName.ts
@@ -3,27 +3,24 @@ import { isDebug } from '../../libs/dev';
 let className: string = '';
 let actionCounter: number = 0;
 
-// In case the same classname by uglify in production mode.
-// Do not use this variable in dev mode with hot reloading.
-// Remember: Do not create class by the same name, or reducer will be override by another one.
-const PROD_CLASS_DICT = {};
+// In case the same classname is compressed even when it's dev environment
+// The program can be look as compressed if the length of classname is less than 2 characters.
+const CLASS_DICT = {};
 
 export const setInstanceName = (name: string, alias: string): string => {
   className = name + (alias ? `.${alias}` : '');
   actionCounter = 0;
 
-  if (!isDebug()) {
-    const dictKey = `dict_${className}`;
+  const dictKey = `dict_${className}`;
 
-    if (PROD_CLASS_DICT[dictKey] === undefined) {
-      PROD_CLASS_DICT[dictKey] = 0;
-    } else {
-      PROD_CLASS_DICT[dictKey] += 1;
-    }
+  if (CLASS_DICT[dictKey] === undefined) {
+    CLASS_DICT[dictKey] = 0;
+  } else if (className.length <= 2 || !isDebug()) {
+    CLASS_DICT[dictKey] += 1;
+  }
 
-    if (PROD_CLASS_DICT[dictKey] > 0) {
-      className += `-${PROD_CLASS_DICT[dictKey]}`;
-    }
+  if (CLASS_DICT[dictKey] > 0) {
+    className += `-${CLASS_DICT[dictKey]}`;
   }
 
   return className;
