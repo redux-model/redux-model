8918e665c6a857ede924c0899c48752ed5dd2886
范文华 <531362022@qq.com>
2019-11-07 17:06:09 +0800

    fix: Correctly import path

--- src/core/action/BaseRequestAction.d.ts
+++ src/core/action/BaseRequestAction.d.ts
@@ -1,8 +1,8 @@
 import { BaseAction } from './BaseAction';
 import { RequestSubscriber } from '../utils/types';
-import { HttpServiceHandle } from '../service/HttpServiceHandle';
+import { HttpServiceBuilder } from '../service/HttpServiceBuilder';
 
-export declare abstract class BaseRequestAction<Data, A extends (...args: any[]) => HttpServiceHandle<Data, Response, Payload, M>, Response, Payload, M> extends BaseAction {
+export declare abstract class BaseRequestAction<Data, A extends (...args: any[]) => HttpServiceBuilder<Data, Response, Payload, M>, Response, Payload, M> extends BaseAction {
   onSuccess<CustomData>(effect: RequestSubscriber<CustomData, Response, Payload>['effect']): RequestSubscriber<CustomData, Response, Payload>;
   onPrepare<CustomData>(effect: RequestSubscriber<CustomData, Response, Payload>['effect']): RequestSubscriber<CustomData, Response, Payload>;
   onFail<CustomData>(effect: RequestSubscriber<CustomData, Response, Payload>['effect']): RequestSubscriber<CustomData, Response, Payload>;
