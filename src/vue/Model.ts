5e4c8a38cca7d494fb8d151e5ab0308a3cda9013
范文华 <531362022@qq.com>
2019-11-11 15:03:00 +0800

    feat: Remove vue-hooks as it's not a product package

--- src/vue/Model.ts
+++ src/vue/Model.ts
@@ -1,14 +1,10 @@
 import { BaseModel } from '../core/BaseModel';
-import { useComputed } from 'vue-hooks';
 import { HttpServiceWithMeta, UseSelector } from '../core/utils/types';
 import { METHOD } from '../core/utils/method';
-import { getState } from '../core/utils/createReduxStore';
 
 export abstract class Model<Data = null> extends BaseModel<Data> {
   protected switchReduxSelector<TState = any, TSelected = any>(): UseSelector<TState, TSelected> {
-    return (selector: (state: TState) => TSelected): TSelected => {
-      return useComputed(() => selector(getState()));
-    };
+    throw new ReferenceError('Do not use hooks');
   }
 
   protected patch<Response>(uri: string): HttpServiceWithMeta<Data, Response, unknown> {
--- src/vue/RequestAction.ts
+++ src/vue/RequestAction.ts
@@ -1,11 +1,9 @@
 import { BaseRequestAction } from '../core/action/BaseRequestAction';
-import { useComputed } from 'vue-hooks';
 import { UseSelector } from '../core/utils/types';
 import { HttpServiceBuilder } from '../core/service/HttpServiceBuilder';
-import { getState } from '../core/utils/createReduxStore';
 
 export class RequestAction<Data, A extends (...args: any[]) => HttpServiceBuilder<Data, Response, Payload, M>, Response, Payload, M> extends BaseRequestAction<Data, A, Response, Payload, M> {
   protected switchReduxSelector<TState = any, TSelected = any>(): UseSelector<TState, TSelected> {
-    return useComputed.bind(getState());
+    throw new ReferenceError('Do not use hooks');
   }
 }
