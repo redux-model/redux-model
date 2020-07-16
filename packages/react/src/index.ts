2df6a428ce876d71e32c27770409f21f55f9f74a
范文华 <531362022@qq.com>
2020-07-16 12:01:17 +0800

    feat: Export type FilterPersist to user

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,4 @@
-export { METHOD, HTTP_STATUS_CODE, Effects, createReduxStore, getStore } from './core';
+export { METHOD, HTTP_STATUS_CODE, Effects, createReduxStore, getStore, FilterPersist } from './core';
 export { Model } from './models/Model';
 export { HttpService, HttpResponse, FetchHandle, HttpCanceler } from './services/HttpService';
 export { PersistGate } from './components/PersistGate';
--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,4 +1,4 @@
 import './stores/storeListener';
-export { METHOD, HTTP_STATUS_CODE, Effects, createReduxStore, getStore } from './core';
+export { METHOD, HTTP_STATUS_CODE, Effects, createReduxStore, getStore, FilterPersist } from './core';
 export { Model } from './models/Model';
 export { HttpService, HttpResponse, FetchHandle, HttpCanceler } from './services/HttpService';
