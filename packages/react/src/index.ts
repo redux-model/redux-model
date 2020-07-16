18299a1c2f2f35825661d22524da6c4a2b184486
范文华 <531362022@qq.com>
2020-07-16 18:57:20 +0800

    fix: Export incorrect createReduxStore() function

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,5 @@
-export { METHOD, HTTP_STATUS_CODE, Effects, createReduxStore, getStore, FilterPersist } from './core';
+export { METHOD, HTTP_STATUS_CODE, Effects, getStore, FilterPersist } from './core';
 export { Model } from './models/Model';
 export { HttpService, HttpResponse, FetchHandle, HttpCanceler } from './services/HttpService';
 export { PersistGate } from './components/PersistGate';
+export { createReduxStore } from './stores/createReduxStore';
