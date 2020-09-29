2f1fba5c8e719d7855e85d19b32622fea0d0a0f5
范文华 <531362022@qq.com>
2020-09-29 17:25:24 +0800

    chore: Compatible with es modules

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,11 +10,16 @@ else
 fi
 
 rm -rf ./build/*
+# commonJS
 yarn tsc
 mv ./build/src ./build/lib
 cp ../../README.md ../../LICENSE ../../CHANGELOG.md ./package.json ./build
+# esModules
+yarn tsc --module ES6
+mv ./build/src ./build/es
 
 ../../node_modules/.bin/public-refactor --src ./src --dist ./build/lib
+../../node_modules/.bin/public-refactor --src ./src --dist ./build/es
 
 # packages/core
 replace 's@\("main": \)"src/index\.ts"@\1"lib/index.js"@' ./build/package.json
