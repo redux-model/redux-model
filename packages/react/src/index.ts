263ac5bab55a1b11ea64b894f2284e30a235a656
范文华 <531362022@qq.com>
2020-09-30 14:37:07 +0800

    feat: Export redux.Store

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,7 +3,7 @@ export { Model } from './models/Model';
 export { HttpService, FetchHandle, HttpCanceler } from './services/HttpService';
 export { PersistGate } from './components/PersistGate';
 export { createReduxStore } from './stores/createReduxStore';
-export { Middleware } from 'redux';
+export { Middleware, Store } from 'redux';
 export { Provider, connect } from 'react-redux';
 export * as redux from 'redux';
 export * as reactRedux from 'react-redux';
