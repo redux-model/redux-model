d0b90bdefb1b33ee9fceb39b3ad09673fadd3d4f
范文华 <531362022@qq.com>
2020-10-10 14:58:24 +0800

    refactor: Move resetStore to global scope

--- docs/zh-cn/reset-store.md
+++ docs/zh-cn/reset-store.md
@@ -1,11 +1,11 @@
 结合业务使用场景，当用户退出登陆时，程序需要手动重置redux中的数据。在浏览器中可以通过刷新页面的方式简单粗暴地释放内存数据。但如果时在App或者小程序中，刷新操作行不通。所以我们来看看模型中如何快速重置数据
 
 ```typescript
-import { Model } from '@redux-model/react';
+import { resetStore } from '@redux-model/react';
 
-Model.resetStore();
+resetStore();
 ```
-就这么简单，模型内置一个静态方法用于重置数据，所有实例化的模型都会被重置回`initialState`的值。您也许还想更细腻一些，某些模型其实存储的是通用的数据，与用户无关，您想在重置的时候保留它们。看看下面的做法：
+就这么简单，模型内置一个静态方法用于重置数据，所有实例化的模型（即使是持久层）都会被重置回`initialState`的值。您也许还想更细腻一些，某些模型其实存储的是通用的数据，与用户无关，您想在重置的时候保留它们。看看下面的做法：
 
 ```typescript
 class TestModel extends Model {
---
+++ packages/core/src/stores/resetStore.ts
@@ -0,0 +1,26 @@
+import { AnyAction } from 'redux';
+import { storeHelper } from '..';
+import ACTION_TYPES from '../utils/actionType';
+
+/**
+ * Reset store and reassign initial state for each model.
+ * ```javascript
+ * logout().then(() => {
+ *   resetStore();
+ * });
+ * ```
+ *
+ * You can keep current state by override method `keepStateOnReset()`
+ * ```javascript
+ * class TestModel extends Model {
+ *   protected keepOnResetStore() {
+ *     return true;
+ *   }
+ * }
+ * ```
+ */
+export const resetStore = (): AnyAction => {
+  return storeHelper.dispatch({
+    type: ACTION_TYPES.reset,
+  });
+};
