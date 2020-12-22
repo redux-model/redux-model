20809711944494f32ef7a7ed565b7a3e7f227081
原罪 <531362022@qq.com>
2020-12-22 12:52:38 +0800

    fix: Forget to export useLocalModel

--- packages/taro/src/index.ts
+++ packages/taro/src/index.ts
@@ -1,5 +1,6 @@
 export { METHOD, HTTP_STATUS_CODE, Subscriptions, getStore, resetStore, FilterPersist } from '@redux-model/core';
 export { Model } from './models/Model';
+export { useLocalModel } from './hooks/useLocalModel';
 export { HttpService, FetchHandle, HttpCanceler } from './services/HttpService';
 export { PersistGate } from './components/PersistGate';
 export { createReduxStore } from './stores/createReduxStore';
--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,6 +1,7 @@
 import './stores/storeListener';
 export { METHOD, HTTP_STATUS_CODE, Subscriptions, getStore, resetStore, FilterPersist } from '@redux-model/core';
 export { Model } from './models/Model';
+export { useLocalModel } from './hooks/useLocalModel';
 export { HttpService, FetchHandle, HttpCanceler } from './services/HttpService';
 export { PersistGate } from './components/PersistGate';
 export { createReduxStore } from './stores/createReduxStore';
