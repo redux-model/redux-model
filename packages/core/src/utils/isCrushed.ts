d09490e326866801ac81f4da1a534b96e0adcbac
原罪 <531362022@qq.com>
2021-07-16 11:59:31 +0800

    fix: Toplevel fail to crush in ESM by terser with default configuration

--- packages/core/src/utils/isCrushed.ts
+++ packages/core/src/utils/isCrushed.ts
@@ -1,7 +1,17 @@
-function CustomModel() {}
+/*
+ * The browsers which don't support Function.name should be considered as compressed.
+ *
+ * @link https://github.com/terser/terser
+ * toplevel (default false) - set to true if you wish to enable top level variable
+ * and function name mangling and to drop unused variables and functions.
+ *
+ * For ESM, don't put CustomModel to toplevel.
+ */
+const name = (() => {
+  function CustomModel() {}
+  return CustomModel.name;
+});
 
-// The browsers which don't support Function.name should be considered as compressed.
-const name = CustomModel.name;
 const crushed = typeof name !== 'string' || name !== 'CustomModel';
 
 export const isCrushed = () => crushed;
